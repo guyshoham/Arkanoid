d3ed7b24f5c91eba9ee468228a97c1f6429a6f99
Guy Shoham <guyshoham28@gmail.com>
2019-03-31 22:04:55 +0300

    unit testing

--- test/LineTest.java
+++ test/LineTest.java
@@ -11,4 +11,114 @@ public class LineTest {
         boolean result = Line.isPointInLine(line, middle);
         assertTrue(result);
     }
+
+    @Test
+    public void start() {
+        Point start = new Point(0, 12);
+        Point end = new Point(5, 0);
+        Line line = new Line(start, end);
+
+        assertEquals(start, line.getStart());
+    }
+
+    @Test
+    public void end() {
+        Point start = new Point(0, 12);
+        Point end = new Point(5, 0);
+        Line line = new Line(start, end);
+
+        assertEquals(end, line.getEnd());
+    }
+
+    @Test
+    public void length() {
+        Point start = new Point(0, 12);
+        Point end = new Point(5, 0);
+        Line line = new Line(start, end);
+
+        assertEquals(13, (int) line.length());
+    }
+
+    @Test
+    public void middle() {
+        Point start = new Point(10, 50);
+        Point end = new Point(30, 40);
+        Point middle = new Point(20, 45);
+        Line line = new Line(start, end);
+
+        assertEquals((int) middle.getX(), (int) line.middle().getX());
+        assertEquals((int) middle.getY(), (int) line.middle().getY());
+    }
+
+    @Test
+    public void getSlope() {
+        Point start = new Point(10, 20);
+        Point end = new Point(5, 5);
+        Line line = new Line(start, end);
+
+        assertEquals(3, (int) line.getSlope());
+    }
+
+    @Test
+    public void getIntercept() {
+        Point start = new Point(10, 20);
+        Point end = new Point(5, 5);
+        Line line = new Line(start, end);
+
+        assertEquals(-10, (int) line.getIntercept());
+    }
+
+    @Test
+    public void isIntersecting() {
+        Line line1 = new Line(new Point(0, 0), new Point(10, 10));
+        Line line2 = new Line(new Point(10, 0), new Point(0, 10));
+
+        assertTrue(line1.isIntersecting(line2));
+    }
+
+    @Test
+    public void intersectionWith() {
+        Line line1 = new Line(new Point(0, 0), new Point(10, 10));
+        Line line2 = new Line(new Point(10, 0), new Point(0, 10));
+        Point point = line1.intersectionWith(line2);
+
+        assertEquals(5, (int) point.getX());
+        assertEquals(5, (int) point.getY());
+
+    }
+
+    @Test
+    public void equals() {
+        Line line1 = new Line(new Point(0, 0), new Point(10, 10));
+        Line line2 = new Line(new Point(10, 0), new Point(0, 10));
+        assertFalse(line1.equals(line2));
+
+        line1 = new Line(new Point(10, 0), new Point(10, 10));
+        line2 = new Line(new Point(10, 0), new Point(10, 10));
+        assertTrue(line1.equals(line2));
+    }
+
+    @Test
+    public void getStart() {
+    }
+
+    @Test
+    public void getEnd() {
+    }
+
+    @Test
+    public void calcSlope() {
+    }
+
+    @Test
+    public void calcIntercept() {
+    }
+
+    @Test
+    public void calcDet() {
+    }
+
+    @Test
+    public void closestIntersectionToStartOfLine() {
+    }
 }
---
+++ test/PointTest.java
@@ -0,0 +1,58 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PointTest {
+
+    @Test
+    public void distance() {
+        Point p1 = new Point(0, 12);
+        Point p2 = new Point(5, 0);
+        int result = (int) p1.distance(p2);
+        assertEquals(13, result);
+
+    }
+
+    @Test
+    public void equals() {
+        Point p1, p2;
+
+        p1 = new Point(2, 2);
+        p2 = new Point(2, -2);
+        assertFalse(p1.equals(p2));
+
+        p1 = new Point(2, 2);
+        p2 = new Point(-2, 2);
+        assertFalse(p1.equals(p2));
+
+        p1 = new Point(2, 2);
+        p2 = new Point(2, 2);
+        assertTrue(p1.equals(p2));
+    }
+
+    @Test
+    public void getX() {
+        Point p = new Point(2, 3);
+        assertEquals(2, (int) p.getX());
+    }
+
+    @Test
+    public void getY() {
+        Point p = new Point(2, 3);
+        assertEquals(3, (int) p.getY());
+    }
+
+    @Test
+    public void isInRect() {
+        Rectangle rect = new Rectangle(new Point(0, 0), 10, 10);
+        Point point;
+
+        point = new Point(0, 0);
+        assertFalse(point.isInRect(rect));
+        assertFalse(point.isInRect(rect.getUpperLeft(), rect.getLowerRight()));
+
+        point = new Point(5, 5);
+        assertTrue(point.isInRect(rect));
+        assertTrue(point.isInRect(rect.getUpperLeft(), rect.getLowerRight()));
+    }
+}
