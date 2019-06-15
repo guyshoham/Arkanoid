f34490c6bede4a0cc47b74548def1ecdc3a96f50
Guy Shoham <guyshoham28@gmail.com>
2019-06-15 16:52:00 +0300

    Part 4: Different Level-Sets

--- src/backend/GameLevel.java
+++ src/backend/GameLevel.java
@@ -144,7 +144,7 @@ public class GameLevel implements Animation {
         blockRight.addToGame(this);
 
         //init death region block
-        Rectangle rectBottom = new Rectangle(new Point(0, 596), 800, 25);
+        Rectangle rectBottom = new Rectangle(new Point(0, 575), 800, 25);
         Block blockBottom = new Block(rectBottom, Color.BLACK);
         blockBottom.addToGame(this);
         blockBottom.addHitListener(ballRemover);
